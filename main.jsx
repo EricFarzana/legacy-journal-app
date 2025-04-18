8fa51e377e9a33461127b03a2baa8df2387f967b
EricFarzana <eric.farzana@gmail.com>
2025-04-18 12:09:52 +0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => <h1>Legacy Journal App</h1>;
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
